7e754e990d65b42baefacd02e73f3a865bf22c4d
SullyDurgin <katsdurgin@gmail.com>
2021-12-03 18:19:56 -0500

    lab done

--- src/Score.jsx
+++ src/Score.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 const Score = (props) => {
-
+  const scores = props.scores.map((ele, index) => {
+    return(
+      <div>
+        <p>Date: {ele.date}</p>
+        <p>Score: {ele.score}</p>
+      </div>
+    )
+  })
 
   return (
 <div>
-
+    {scores}
 </div>
 
   )
